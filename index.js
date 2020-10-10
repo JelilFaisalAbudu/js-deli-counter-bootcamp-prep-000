261b0fce5717248c838c3e7b91fa66f232787489
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-10 17:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function takeANumber(people, person) {
   
   return `Welcome, ${person}. You are number ${people.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  return katzDeliLine.shift();
+}
