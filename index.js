5073676c15b3adaf0199ada97b8079a95b6afd8f
Jelil Abudu <abudujelilfaisal@gmail.com>
2020-10-10 18:04:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function currentLine(katzDeliLine) {
   else {
     let i = 0;
     do {
-      currLine += ` ${i + 1}. ${katzDeliLine[i]}`;
+      currLine += ` ${i + 1}. ${katzDeliLine[i]},`;
       i++;
     } while (i < katzDeliLine.length);
   }
